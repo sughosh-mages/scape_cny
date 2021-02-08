3b2fab3c803b1608e0092134f812b87f3f0c563c
cindygozali <cindygozali@users.noreply.github.com>
2021-02-08 21:40:55 +0700

    Fix audio is not playing, update AFrame to version 1.2.0

--- js/mages-institute.js
+++ js/mages-institute.js
@@ -1,50 +1,60 @@
-AFRAME.registerComponent("videohandler", {
-  init: function () {
-      var marker = this.el;
+// AFRAME.registerComponent("videohandler", {
+//   init: function () {
+//       var marker = this.el;
 
-      this.vid = document.querySelector("#vid");
-      var playButton = document.getElementById("playButton");
+//       this.vid = document.querySelector("#vid");
+//       var playButton = document.getElementById("playButton");
 
-      marker.addEventListener('markerFound', function () {
-          this.vid.play();
-          playButton.style.backgroundImage = "url('./textures/stop.png')";
-          playButton.visibility = "visible";
-      }.bind(this));
+//       marker.addEventListener('markerFound', function () {
+//           this.vid.play();
+//           playButton.style.backgroundImage = "url('./textures/stop.png')";
+//           playButton.visibility = "visible";
+//       }.bind(this));
 
-      marker.addEventListener('markerLost', function() {
-          this.vid.pause();
-          this.vid.currentTime = 0;
-          playButton.visibility = "hidden";
-      }.bind(this));
-  }
-});
+//       marker.addEventListener('markerLost', function() {
+//           this.vid.pause();
+//           this.vid.currentTime = 0;
+//           playButton.visibility = "hidden";
+//       }.bind(this));
+//   }
+// });
 
 AFRAME.registerComponent("markerhandler", {
   init: function () {
-      var marker = this.el;
-      var model = document.querySelector("#emodel");
+    var marker = this.el;
+    var model = document.querySelector("#emodel");
+    var music = document.querySelector("#emusic");
+    music.components.sound.playSound();
+    // var playButton = document.getElementById("playButton");
 
-      marker.addEventListener('markerFound', function () {
-        // this.model.setAttribute('animation-mixer', {loop: "once", clampWhenFinished: "true"});
-        // model.setAttribute('animation-mixer', {timeScale: 1});
-        model.setAttribute('animation-mixer', {clip: "*"});
-      }.bind(this));
+    marker.addEventListener('markerFound', function () {
+      // this.model.setAttribute('animation-mixer', {loop: "once", clampWhenFinished: "true"});
+      // model.setAttribute('animation-mixer', {timeScale: 1});
+      model.setAttribute('animation-mixer', {clip: "*"});
+    }.bind(this));
 
-      marker.addEventListener('markerLost', function() {
-        // this.model.removeAttribute('animation-mixer');
-        // model.setAttribute('animation-mixer', {timeScale: 0});
-        model.setAttribute('animation-mixer', {clip: "temp"});
-      }.bind(this));
+    marker.addEventListener('markerLost', function() {
+      // this.model.removeAttribute('animation-mixer');
+      // model.setAttribute('animation-mixer', {timeScale: 0});
+      model.setAttribute('animation-mixer', {clip: "temp"});
+    }.bind(this));
   }
 });
 
 window.onload = function () {
   // var model = document.querySelector("#test");
-  // model.setAttribute('animation-mixer', {timeScale: 1});
+  // // model.setAttribute('animation-mixer', {timeScale: 1});
 
   // model.addEventListener('animation-finished', function () {
   //   console.log("animation-finished");
   //   // model.setAttribute('animation-mixer', {timeScale: 0});
+  //   model.setAttribute('animation-mixer', {clip: " "});
+  //   model.setAttribute('animation-mixer', {clip: "*"});
+  //   // model.components.animation-mixer.playAction();
+  //   // console.log(model.components.animation-mixer.mixer);
+  //   // model.components.animation-mixer.playAnim();
+  //   // model.components.animation-mixer.mixer.clipAction(clip, model).play();
+  //   console.log("animation-finished-end");
   // });
 
   // model.addEventListener('animation-loop', function () {
